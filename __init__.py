428195993f7c4e6d3c9d665701b387991aa78d16
ApurvaThakur <42955904+ApurvaThakur@users.noreply.github.com>
2020-08-09 01:46:19 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,15 +1,23 @@
 from flask import Flask, redirect, url_for, render_template, request
 app = Flask(__name__)
 
-@app.route("/")
+@ app.route("/")
 def home():
     return render_template('index.html')
 
+@ app.route("/login")
 def login():
     if request.method == "POST":
-	username = request.form["name"]
-	return redirect(url_for("user", user=username))
-    else:
-	return render_template("login.html")
+       username = request.form["username"]
+       return redirect(url_for("user", user = username))
+    else :
+       return render_template("login.html")
+
 def user(usr):
-    return f"<h1>Welcome {usr}</h1>"
+    return "<h1>Welcome {usr}</h1>"
+
+if __name__ == "__main__":
+    app.debug = True
+    host = os.environ.get('IP','0.0.0.0')
+    port = int(os.environ.get('PORT',80))
+    app.run(host=host, port=port)
