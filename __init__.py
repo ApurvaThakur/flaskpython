32414824bfb7381b553091ba81da69640b7084f3
ApurvaThakur <42955904+ApurvaThakur@users.noreply.github.com>
2020-08-09 16:35:35 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -9,15 +9,8 @@ def home():
 def login():
     if request.method == "POST":
        user = request.form.get["name"]
-       return redirect(url_for("user", usr = user))
     else :
        return render_template("login.html")
-@ app.route("/<usr>")
-def user(usr):
-    return "<h1> Welcome {usr}</h1>"
 
 if __name__ == "__main__":
-    #app.debug = True
-    #host = os.environ.get('IP','0.0.0.0')
-    #port = int(os.environ.get('PORT',80))
     app.run(debug=True, host='0.0.0.0', port=8080)
